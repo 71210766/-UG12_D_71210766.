8289708afeb766bf9ac4ccef85bbad1ed9382d4a
71210766 <93719122+71210766@users.noreply.github.com>
2022-12-08 17:14:36 +0800

    Update 2_D_71210766.py

--- 2_D_71210766.py
+++ 2_D_71210766.py
@@ -5,12 +5,9 @@ print("2. Pembagian")
 pilihan = int(input("Masukkan model matematika yang diinginkan 1/2 : "))
 angka = int(input("Menampilkan table matematika dari angka : "))
 if pilihan == 1 :
-    for i in range(1, 10):
+    for i in range(1, 11):
         sama_dengan = angka * i
         print(angka, "x", i, "=", sama_dengan)
-    for i in range(3, 10):
-        sama_dengan2 = angka * i 
-        print(angka, "x", i, "=", sama_dengan2) 
 elif pilihan == 2:
     for i in range(50, 66):
         sama_dengan = i / angka

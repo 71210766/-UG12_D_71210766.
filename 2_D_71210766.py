e4ea0527fcd9ab73fbf439c3aefcdf4094d35903
71210766 <93719122+71210766@users.noreply.github.com>
2022-12-08 17:07:08 +0800

    Update 2_D_71210766.py

--- 2_D_71210766.py
+++ 2_D_71210766.py
@@ -2,10 +2,8 @@ print("~~~~Table Matematika Nich~~~~")
 print("Pilihan Model Matematika")
 print("1. Perkalian")
 print("2. Pembagian")
-
 pilihan = int(input("Masukkan model matematika yang diinginkan 1/2 : "))
 angka = int(input("Menampilkan table matematika dari angka : "))
-
 if pilihan == 1 :
     for i in range(1, 10):
         sama_dengan = angka * i
@@ -14,12 +12,9 @@ if pilihan == 1 :
         sama_dengan2 = angka * i 
         print(angka, "x", i, "=", sama_dengan2) 
 elif pilihan == 2:
-    for i in range(1, 10):
-        sama_dengan = angka / i
-        print(angka, ":", i, "=", sama_dengan)
-    for i in range(3, 10):
-        sama_dengan2 = angka / i 
-        print(angka, ":", i, "=", sama_dengan2) 
+    for i in range(50, 66):
+        sama_dengan = i / angka
+        print(i, ":", angka, "=", sama_dengan)
 else :
     print("Pilihan tidak tersedia, jangan ngasal!")
 
